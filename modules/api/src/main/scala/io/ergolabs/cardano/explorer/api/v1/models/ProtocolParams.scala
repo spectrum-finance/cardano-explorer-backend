8c62c45d0792a86ab3cf8d856d5c196885a5adb7
Bromel777 <alexbromel@gmail.com>
2023-04-10 12:13:28 +0300

    costPerSize -> coinsPerUtxoByte

--- modules/api/src/main/scala/io/ergolabs/cardano/explorer/api/v1/models/ProtocolParams.scala
+++ modules/api/src/main/scala/io/ergolabs/cardano/explorer/api/v1/models/ProtocolParams.scala
@@ -25,7 +25,7 @@ final case class ProtocolParams(
   poolPledgeInfluence: Double,
   monetaryExpansion: Double,
   treasuryCut: Double,
-  utxoCostPerWord: Option[Long],
+  coinsPerUtxoByte: Option[Long],
   costModels: Map[String, Map[String, Long]],
   executionUnitPrices: ExecutionUnitPrices,
   maxTxExecutionUnits: ExecutionUnits,
@@ -39,31 +39,32 @@ object ProtocolParams {
 
   implicit val schema: Schema[ProtocolParams] = Schema.derived[ProtocolParams]
 
-  def fromEpochParams(epochParams: EpochParams, costModelsMap: Map[String, Map[String, Long]]): ProtocolParams = ProtocolParams(
-    protocolVersion        = ProtocolVersion(epochParams.majorVersion, epochParams.minorVersion),
-    decentralization       = epochParams.decentralization,
-    extraPraosEntropy      = epochParams.extraEntropy,
-    maxBlockHeaderSize     = epochParams.maxBlockHeaderSize,
-    maxBlockBodySize       = epochParams.maxBlockSize,
-    maxTxSize              = epochParams.maxTxSize,
-    txFeeFixed             = epochParams.txFeeFixed,
-    txFeePerByte           = epochParams.txFeePerByte,
-    minUTxOValue           = epochParams.minUtxoValue,
-    stakeAddressDeposit    = epochParams.keyDeposit,
-    stakePoolDeposit       = epochParams.poolDeposit,
-    minPoolCost            = epochParams.minPoolCost,
-    poolRetireMaxEpoch     = epochParams.maxEpoch,
-    stakePoolTargetNum     = epochParams.optimalPoolCount,
-    poolPledgeInfluence    = epochParams.influence,
-    monetaryExpansion      = epochParams.monetaryExpansion,
-    treasuryCut            = epochParams.treasuryGrowthRate,
-    utxoCostPerWord        = epochParams.costPerSize,
-    costModels             = costModelsMap,
-    executionUnitPrices    = ExecutionUnitPrices(epochParams.priceStep, epochParams.priceMemory),
-    maxTxExecutionUnits    = ExecutionUnits(epochParams.maxTxExSteps, epochParams.maxTxExMem),
-    maxBlockExecutionUnits = ExecutionUnits(epochParams.maxBlockExSteps, epochParams.maxBlockExMem),
-    maxValueSize           = epochParams.maxValSize,
-    collateralPercentage   = epochParams.collateralPercent,
-    maxCollateralInputs    = epochParams.maxCollateralInputs
-  )
+  def fromEpochParams(epochParams: EpochParams, costModelsMap: Map[String, Map[String, Long]]): ProtocolParams =
+    ProtocolParams(
+      protocolVersion        = ProtocolVersion(epochParams.majorVersion, epochParams.minorVersion),
+      decentralization       = epochParams.decentralization,
+      extraPraosEntropy      = epochParams.extraEntropy,
+      maxBlockHeaderSize     = epochParams.maxBlockHeaderSize,
+      maxBlockBodySize       = epochParams.maxBlockSize,
+      maxTxSize              = epochParams.maxTxSize,
+      txFeeFixed             = epochParams.txFeeFixed,
+      txFeePerByte           = epochParams.txFeePerByte,
+      minUTxOValue           = epochParams.minUtxoValue,
+      stakeAddressDeposit    = epochParams.keyDeposit,
+      stakePoolDeposit       = epochParams.poolDeposit,
+      minPoolCost            = epochParams.minPoolCost,
+      poolRetireMaxEpoch     = epochParams.maxEpoch,
+      stakePoolTargetNum     = epochParams.optimalPoolCount,
+      poolPledgeInfluence    = epochParams.influence,
+      monetaryExpansion      = epochParams.monetaryExpansion,
+      treasuryCut            = epochParams.treasuryGrowthRate,
+      coinsPerUtxoByte       = epochParams.coinsPerUtxoByte,
+      costModels             = costModelsMap,
+      executionUnitPrices    = ExecutionUnitPrices(epochParams.priceStep, epochParams.priceMemory),
+      maxTxExecutionUnits    = ExecutionUnits(epochParams.maxTxExSteps, epochParams.maxTxExMem),
+      maxBlockExecutionUnits = ExecutionUnits(epochParams.maxBlockExSteps, epochParams.maxBlockExMem),
+      maxValueSize           = epochParams.maxValSize,
+      collateralPercentage   = epochParams.collateralPercent,
+      maxCollateralInputs    = epochParams.maxCollateralInputs
+    )
 }
