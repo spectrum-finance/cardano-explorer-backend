e878217a3a80d7dcda2a081c1076d604301534d3
Bromel777 <alexbromel@gmail.com>
2024-12-17 15:46:42 +0300

    Add temporal solution for preventing metrics spam

--- modules/api/src/main/scala/io/ergolabs/cardano/explorer/api/graphite/MetricsMiddleware.scala
+++ modules/api/src/main/scala/io/ergolabs/cardano/explorer/api/graphite/MetricsMiddleware.scala
@@ -16,8 +16,16 @@ object MetricsMiddleware {
 
   final class MetricsMiddleware[F[_]: Monad: Clock](metrics: Metrics[F]) {
 
+    // todo: temporal solution to prevent spamming keys with outputs.
+
+    val outputsRegex = "[\"outputs.\"].*".r
+
     def middleware(routes: HttpRoutes[F]): HttpRoutes[F] = Kleisli { req =>
-      val key = req.pathInfo.renderString.replaceAll("/", ".").replaceAll(":", ".").drop(1)
+      val key = outputsRegex.replaceAllIn(req.pathInfo
+        .renderString
+        .replaceAll("/", ".").replaceAll(":", ".")
+        .drop(1), ".outputs")
+
       for {
         start  <- OptionT.liftF(Clock[F].realTime(TimeUnit.MILLISECONDS))
         resp   <- routes(req)
